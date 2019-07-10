5b662e2399b918310d223d056b294f85973c8379
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-07-10 21:55:04 +0100

    Added Javascript to make a request to get a book and display the contents on the webpage

---
+++ SearchString.js
@@ -0,0 +1,29 @@
+//Load a book from disk
+function loadBook(filename,displayName){
+    let currentBook = "";
+    let url = "books/"+filename;
+
+    //reset the UI, and the searchstat footer.
+    document.getElementById("fileName").innerHTML = displayName;
+    document.getElementById("searchstat").innerHTML = "";
+    document.getElementById("keyword").value = "";
+
+    //create a http server request to load the books.
+    var xhr = new XMLHttpRequest();
+    // true to run async
+    xhr.open("GET", url, true);
+    xhr.send();
+    
+    //wait for book to load. And set it in the scroll box, reset to top.
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState == 4 && xhr.status == 200){
+            currentBook = xhr.responseText;
+
+            document.getElementById("fileContent").innerHTML = currentBook;
+
+            var elmnt = document.getElementById("fileContent");
+            elmnt.scrollTop = 0;
+
+        }
+    }
+}
