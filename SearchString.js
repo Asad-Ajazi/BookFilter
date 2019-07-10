00ff9797e5ffe02709a84eed0402f7d661ce8d79
Asad Ajazi <AsadAjazi.work@gmail.com>
2019-07-10 23:46:42 +0100

    Added a word filter system to filter out stopwords

--- SearchString.js
+++ SearchString.js
@@ -46,9 +46,13 @@ function getDocStats(fileContent){
     let wordArray = text.match(/\b\S+\b/g);
     let wordDictionary = {};
 
+    var uncommonWords = [];
+    // filter out the uncommon words
+    uncommonWords = filterStopwords(wordArray);
+
     //count every word in the wordArray, add 1 to count if found, else add to the dictionary.
-    for(let word in wordArray){
-        let wordValue = wordArray[word];
+    for(let word in uncommonWords){
+        let wordValue = uncommonWords[word];
         if(wordDictionary[wordValue] > 0){
             wordDictionary[wordValue] += 1;
         }
@@ -75,10 +79,9 @@ function ULTemplate(items,element){
     let resultsHTML = "";
 
     for(i=0;i<items.length-1;i++){
-        //resultsHTML += templateHTML.replace('{{val}}',items[i][0]+ " : "+ items[i][1] + "time(s)");
-        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
+        resultsHTML += templateHTML.replace('{{val}}',items[i][0]+ " : "+ items[i][1] + "time(s)");
+        //resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
     }
-
     element.innerHTML = resultsHTML;
 }
 
@@ -91,5 +94,29 @@ function sortProperties(obj){
         return second[1] - first[1];
     });
     return rtnArray;
+}
 
+// filter out the overly common stopwords.
+function filterStopwords(wordArray){
+    var commonWords = getStopWords();
+    var commonObj = {};
+    var uncommonArr = [];
+    
+    for (i = 0; i < commonWords.length; i++) {
+          commonObj[commonWords[i].trim()] = true;
+    }
+
+    for (i = 0; i < wordArray.length; i++){
+        word = wordArray[i].trim().toLowerCase();
+        if(!commonObj[word]){
+            uncommonArr.push(word);
+        }
+    }
+    return uncommonArr;
+
+}
+
+// A generic list of words that are very common and will not be included in the frequest word section.
+function getStopWords(){
+    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
 }
